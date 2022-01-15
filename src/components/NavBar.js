6f21fe19244faeab41c93d67fa1cd75a604cac94
jose Tovar <jose.tovar102397@gmail.com>
2022-01-14 19:40:22 -0700

    Add elements needed in nav bar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,15 @@
 import React from "react";
-import './NavBar.css'
+import './NavBar.css';
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="nav-bar">
       <h1>Bibliotheca</h1>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/">My Books</NavLink>
+      <input type="text"></input>
+      <button>Search</button>
     </nav>
   )
 }
