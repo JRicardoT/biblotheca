24dba9d7f067df351baed8e52fdda1293787f5c1
jose Tovar <jose.tovar102397@gmail.com>
2022-01-14 23:16:12 -0700

    Add state to app component to store books data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import './App.css';
-import NavBar from './NavBar';
-import BooksContainer from './BooksContainer';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import NavBar from "./NavBar";
+import BooksContainer from "./BooksContainer";
+import { getBooks } from "../apiCalls";
 
 const App = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect (() => {
+    getBooks()
+    .then(data => setBooks(data.results.books))
+  }, [])
+
+  console.log("outside of effect", books)
   return (
     <main>
       <NavBar />
