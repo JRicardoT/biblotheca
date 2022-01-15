cf318ce7d199d785920dccc2c4b412132281d1a8
jose Tovar <jose.tovar102397@gmail.com>
2022-01-14 19:45:53 -0700

    Add books container component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './NavBar';
+import BooksContainer from './BooksContainer';
 
 const App = () => {
   return (
     <main>
       <NavBar />
+      <BooksContainer />
     </main>
   );
 }
---
+++ src/components/BooksContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BooksContainer = () => {
+  return (
+    <section>
+      
+    </section>
+  )
+}
+
+export default BooksContainer;
