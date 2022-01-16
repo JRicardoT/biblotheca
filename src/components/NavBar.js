4bbaa4b5cecee5fc717a656a979180292f39c749
jose Tovar <jose.tovar102397@gmail.com>
2022-01-16 12:11:52 -0700

    Add jsx elements to nav bar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,11 +5,15 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="nav-bar">
-      <h1>Bibliotheca</h1>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/">My Books</NavLink>
-      <input type="text"></input>
-      <button>Search</button>
+      <div className="title-links">
+        <h1>Bibliotheca</h1>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">My Books</NavLink>       
+      </div>
+      <form className="search-bar-container">
+        <input type="text"></input>
+        <button className="submit-search">Search</button>
+      </form>
     </nav>
   )
 }
