30f32c20b0347f0ec17b5d032a0c9edb33de2076
jose Tovar <jose.tovar102397@gmail.com>
2022-01-16 12:05:44 -0700

    Add class names to jsx elements in book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Book.css"
 
 const Book = ({ author, bookImage, bookBuyLink, description, publisher, title }) => {
   // console.log('author', author, 'image', bookImage, 'link', bookBuyLink, 'description', description, 'publisher', publisher, 'title', title)
   return (
-    <section>
-      <img src={bookImage} alt='Book cover'></img>
-      <div>
+    <section className="book-section">
+      <img className="book-cover" src={bookImage} alt='Book cover'></img>
+      <div className="book-title">
         <h2>{title}</h2>
         <p>By {author}</p>
       </div>
