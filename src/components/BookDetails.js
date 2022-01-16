ecbaa01355ede2ba23e43866fb3f637775782cf9
jose Tovar <jose.tovar102397@gmail.com>
2022-01-16 15:51:29 -0700

    Render book details in details page

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const BookDetails = () => {
+const BookDetails = ({ books }) => {
+  const bookId = useParams().id;
+  const bookToDisplay = books.find(book => book.title === bookId);
+  console.log(bookToDisplay);
+
+  // author={book.author}
+  // bookImage={book.book_image}
+  // bookBuyLink={book.buy_links[0].url}
+  // description={book.description}
+  // publisher={book.publisher}
+  // title={book.title}
+
   return (
     <section>
-      
+      <h3>{bookToDisplay.title}</h3>
+      <img src={bookToDisplay.book_image} alt="book-cover"></img>
+      <h4>By {bookToDisplay.author}</h4>
+      <p>Publisher: {bookToDisplay.publisher}</p>
+      <a href={bookToDisplay.buy_links[0].url} target="_blank" rel="noreferrer"><button className="buy-book-button">Buy Book</button></a>
+      <h5>Description</h5>
+      <p>{bookToDisplay.description}</p>
     </section>
   )
 }
