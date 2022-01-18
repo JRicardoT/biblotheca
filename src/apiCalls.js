ca3e2cf3a43e5a0feacc86bcd401f61b91c45d50
jose Tovar <jose.tovar102397@gmail.com>
2022-01-18 12:14:14 -0700

    Delete comments an unused lines of code

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,6 @@
 const getBooks = (endPoint) => {
-  return fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${endPoint || 'combined-print-nonfiction'}.json?api-key=`)
-  // https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=
+  return fetch(`https://api.nytimes.com/svc/books/v3/lists/current/${endPoint || 'combined-print-nonfiction'}.json?api-key=MFgFnfP7N2jFFaR5GYNLL5X6B00bK9jl`)
   .then(res => res.json())
 }
-
-// endpoints
-// 'combined-print-and-e-Book-fiction' / fiction
-// 'combined-print-and-e-book-nonfiction' / nonfiction
-// 'audio-nonfiction' - audio
-// 'childrens-middle-grade' - kids
-// 'manga' - manga
-// 'games-and-activities' - games & activities
 
 export { getBooks }
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import NavBar from "./NavBar";
-import BooksContainer from "./BooksContainer";
-import BookDetails from "./BookDetails";
-import { getBooks } from "../apiCalls";
-import { Route, Routes } from "react-router-dom";
-import FavoriteBooks from "./FavoriteBooks";
-import NonFictionContainer from "./NonFictionContainer";
-// import { categories } from "../utils";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import NavBar from './NavBar';
+import BooksContainer from './BooksContainer';
+import BookDetails from './BookDetails';
+import { getBooks } from '../apiCalls';
+import { Route, Routes } from 'react-router-dom';
+import FavoriteBooks from './FavoriteBooks';
 
 const App = () => {
 
@@ -16,16 +14,7 @@ const App = () => {
     const storedBooks = localStorage.getItem('favoriteBooks');
     return storedBooks ? JSON.parse(storedBooks) : [];
   });
-  // const [url, setUrl] = useState(`https://api.nytimes.com/svc/books/v3/lists/current/combined-print-nonfiction.json?api-key=M4w0uEsAYWmqh4MueC7oRCv9eOIZbkkU`)
-  // const [urlEndPoint, setUrlEndPoint] = useState(() => {
-  //   const storedEndPoint = JSON.parse(localStorage.getItem('endPoint'));
-  //   return storedEndPoint;
-  // });
-   const [urlEndPoint, setUrlEndPoint] = useState();
-
-  // useEffect(() => {
-  //   localStorage.setItem('endPoint', JSON.stringify(urlEndPoint));
-  // }, [urlEndPoint])
+  const [urlEndPoint, setUrlEndPoint] = useState();
 
   useEffect(() => {
     getBooks(urlEndPoint)
@@ -49,22 +38,12 @@ const App = () => {
   }
 
   const updateBooks = (endPoint) => {
-    // console.log(endPoint);
     setUrlEndPoint(endPoint);
   }
 
   const displayHome = (endPoint) => {
-    // console.log(event.target.id)
     setUrlEndPoint(endPoint)
   }
-
-  // const routeToCategories = categories.map(category => {
-  //   return (
-  //     <Route path={`/${category.name.toLowerCase()}`} element={<BooksContainer />} />
-  //   )
-  // });
-  // console.log(books)
-  console.log("hi")
   
   return (
     <main>
